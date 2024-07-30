864590f9082ae91eabcb1d3701ea34e806e46b0a
SerhatAker <81346603+serhataker@users.noreply.github.com>
2024-07-30 23:21:36 +0300

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    public class Person
+    {
+
+        private string name;
+        private DateTime birthDay;
+        private string surname;
+
+        public string Name
+
+
+        {
+            get  {return name; }
+
+            set {
+                name = value;
+            }
+        }
+
+        public DateTime BirthDay
+        {
+
+            get { return birthDay; }
+
+            set
+            {
+                DateTime minDate = new DateTime(1900, 1, 1);
+
+                if (value>minDate)
+                {
+                    birthDay = value;
+                }
+            }
+
+           
+                
+                
+                
+        }
+        public string Surname 
+        
+        {
+            get { return surname; 
+            } set
+                
+
+            {surname=value; 
+            }
+        
+        
+        }
+    } 
+
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Channels;
+namespace FirstClass
+{
+
+    class First
+    {
+
+        static void Main(string[] args)
+        {
+
+            Person student1 = new Person();// create the instance for the student1
+            student1.Name = "Ali";//input the student1 name
+            student1.Surname = "Osmanaga"; //input the student1 surname
+            student1.BirthDay= new DateTime(1993,1,1); //input the student1 birthday 
+
+            Console.WriteLine($"Student Info: Name:{student1.Name} Surename:{student1.Surname} BirthDate:{student1.BirthDay:dd:MM:yyyy}");// write the console we give the date format for the handle birthday
+           
+            
+            Person student2 = new Person();// create the instance for the student2
+            student2.Name = "Hayri";
+            student2.Surname = "Hakverdi";
+            student2.BirthDay= new DateTime(1998,1,5);
+            
+            Console.WriteLine($"Student Info: Name:{student2.Name} Surename:{student2.Surname} BirthDate:{student2.BirthDay:dd:MM:yyyy}"); 
+          
+            
+            Person teacher1 = new Person();// create the instance for the tecaher1
+            teacher1.Name = "Arslan";
+            teacher1.Surname = "Süt";
+            teacher1.BirthDay= new DateTime(1991,2,5);
+            
+            Console.WriteLine($"Teacher Info: Name:{teacher1.Name} Surename:{teacher1.Surname} BirthDate:{teacher1.BirthDay:dd:MM:yyyy}"); Person 
+
+            teacher2 = new Person();// create the instance for the tecaher2
+            teacher1.Name = "Arslan";
+            teacher2.Name = "Halil";
+            teacher2.Surname = "Kahraman";
+            teacher2.BirthDay= new DateTime(1981,10,02);
+            
+            Console.WriteLine($"Teacher Info: Name:{teacher2.Name} Surename:{teacher2.Surname} BirthDate:{teacher2.BirthDay:dd:MM:yyyy}");
+        }
+    }
+
+}
